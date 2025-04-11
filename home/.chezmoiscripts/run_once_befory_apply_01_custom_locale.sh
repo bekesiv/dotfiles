a6f3bacc8c20e7c10ed8a71717328ce9ceed7a7b
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-11 13:24:12 +0200

    Refactor locale handling in run_once_befory_apply_01_custom_locale.sh for consistency

--- home/.chezmoiscripts/run_once_befory_apply_01_custom_locale.sh
+++ home/.chezmoiscripts/run_once_befory_apply_01_custom_locale.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-locale_dir="/usr/share/i18n/locales"
-SRC="$locale_dir/hu_HU"
-DST="$locale_dir/hu_HU_custom"
+LOCALE_DIR="/usr/share/i18n/locales"
 PATCH="$CHEZMOI_SOURCE_DIR/files/locale-fix.sed"
+SRC="$LOCALE_DIR/hu_HU"
+DST="$LOCALE_DIR/hu_HU_custom"
 
 if [ ! -f "$DST" ]; then
   echo "Generating $DST from $SRC..."
-  sed -f "$PATCH" "$SRC" | sudo tee "$DST" > /dev/null
+  sed -E -f "$PATCH" "$SRC" | sudo tee "$DST" > /dev/null
   sudo localedef -i hu_HU_custom -f UTF-8 hu_HU.UTF-8@custom
   echo "Custom locale changed to $DST"
 else
