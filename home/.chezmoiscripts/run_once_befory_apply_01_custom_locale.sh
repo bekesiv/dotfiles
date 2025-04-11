2ef06e53035c45941a8baf9bf745abc696401ee6
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-11 15:18:17 +0200

    Replace sed script in run_once_befory_apply_01_custom_locale.sh and remove locale-fix.sed for consistency

--- home/.chezmoiscripts/run_once_befory_apply_01_custom_locale.sh
+++ home/.chezmoiscripts/run_once_befory_apply_01_custom_locale.sh
@@ -9,7 +9,9 @@ DST="$LOCALE_DIR/hu_HU_custom"
 
 if [ ! -f "$DST" ]; then
   echo "Generating $DST from $SRC..."
-  sed -E -f "$PATCH" "$SRC" | sudo tee "$DST" > /dev/null
+  sed -E -e 's/(decimal_point[[:space:]]+\"),\"/\1.\"/g' \
+         -e 's/(thousands_sep[[:space:]]+\").\"/\1 \"/g' \
+         "$SRC" | sudo tee "$DST" > /dev/null
   sudo localedef -i hu_HU_custom -f UTF-8 hu_HU.UTF-8@custom
   echo "Custom locale changed to $DST"
 else
--- home/files/locale-fix.sed
+++
@@ -1,4 +0,0 @@
-s/^\(mon_decimal_point[[:space:]]\+\)","/\1"./"
-s/^\(mon_thousands_sep[[:space:]]\+\)"\."/\1"/"
-s/^\(decimal_point[[:space:]]\+\)","/\1"./"
-s/^\(thousands_sep[[:space:]]\+\)"\."/\1"/"
