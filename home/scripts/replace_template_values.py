c1343a00df3d2b7018ccebc853815f2e3c22168f
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-13 01:12:38 +0200

    Enhance template processing in replace_template_values.py to include configuration value replacements and improve handling of secrets. Update history size in dot_bashrc template and add new dot_mediainfo-gui configuration file.

--- home/scripts/replace_template_values.py
+++ home/scripts/replace_template_values.py
@@ -41,6 +41,24 @@ def run_command(command: str) -> str:
     result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
     return result.stdout
 
+def process_template_file(template_file: str, config: Dict[str, Any]) -> None:
+    """Processes a template file and replaces placeholders with values from the configuration."""
+    print (f"Processing template file: {template_file}")
+    with open(template_file, 'r') as file:
+        content = file.read()
+
+    for key, value in reversed_config.items():
+        if value.startswith('$secrets'):
+            if key in content:
+                if not content.startswith('{{- $secrets := ("encrypted_dot_secrets.age" | include | decrypt | fromJson) -}}\n'):
+                    content = '{{- $secrets := ("encrypted_dot_secrets.age" | include | decrypt | fromJson) -}}\n' + content
+            content = content.replace(key, f"{{{{ {value} }}}}")
+        else:
+            content = content.replace(key, f"{{{{ .{value} }}}}")
+
+    with open(template_file, 'w') as file:
+        file.write(content)
+
 if __name__ == "__main__":
     print("Getting configuration...")
     command = "chezmoi data"
@@ -48,10 +66,10 @@ if __name__ == "__main__":
 
     cf = configuration['chezmoi']['config']
     command = f'age -i {cf["age"]["identity"]} -d {cf["sourceDir"]}/home/encrypted_dot_secrets.age'
-    configuration.update(json.loads(run_command(command)))
+    configuration['$secrets'] = json.loads(run_command(command))
 
     for i in excludes:
-        del(configuration['chezmoi'][i])
+        del configuration['chezmoi'][i]
 
     flattened_config = flatten(configuration)
     reversed_config = reverse_dict(flattened_config)        
@@ -61,12 +79,5 @@ if __name__ == "__main__":
     command = f'find {flattened_config["chezmoi.sourceDir"]} -type f | grep -E ".tmpl$"'
     templates = run_command(command)
 
-    # for template_file in templates.strip().split('\n'):
-    #     with open(template_file, 'r') as file:
-    #         content = file.read()
-
-    #     for key, value in reversed_config.items():
-    #         content = content.replace(key, value)
-
-    #     with open(template_file, 'w') as file:
-    #         file.write(content)
+    for template_file in templates.strip().split('\n'):
+        process_template_file(template_file, reversed_config)
