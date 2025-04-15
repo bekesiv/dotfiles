2d9003866eb96370714e55b221753f8394727d60
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-16 01:41:30 +0200

    Fix GPG key installation path in run_once_after script

--- home/.chezmoiscripts/run_once_after_.chezmoitmp_home_Alexx2000.asc.sh
+++ home/.chezmoiscripts/run_once_after_.chezmoitmp_home_Alexx2000.asc.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 SRC="$HOME/.chezmoitmp/home_Alexx2000.asc"
-MID="${SRC%.asc}.gpg"
 DST="/etc/apt/trusted.gpg.d/home_Alexx2000.gpg"
 
 echo "🔄 Installing DoubleCDM key..."
 ls -la ~/.chezmoitmp
 echo "Dearmoring GPG Key..."
 sudo gpg -v --dearmor "$SRC"
-sudo install -v -o root -g root -m 644 "$MID" "$DST" || echo "Key downloaded and installed: $DST"
+sudo install -v -o root -g root -m 644 "$SRC.gpg" "$DST" || echo "Key downloaded and installed: $DST"
