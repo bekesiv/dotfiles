bca01ad9742043d1a2d739719b33dc595fe60961
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-16 22:00:17 +0200

    Add scripts for managing APT repositories and update sources.list entries

---
+++ home/.chezmoiscripts/run_once_after_apply_10_install_ecalc .sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# eCalc
+github_dir="$HOME/work/github"
+if [ ! -d "$github_dir" ]; then
+    mkdir -p "$github_dir"
+    cd "$github_dir"
+    git clone git@github.com:bekesiv/ecalc.git
+    cd ecalc
+    ./make_install.sh   
+fi
