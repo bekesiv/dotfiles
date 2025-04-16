899961fd4d6bebdfeb066c0c5b362fecbf82d209
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-16 07:16:22 +0200

    Add scripts for custom locale generation and APT package management; remove obsolete convert script

--- home/.chezmoiscripts/run_once_after_.chezmoitmp_home_Alexx2000.asc.sh
+++ home/.chezmoiscripts/run_once_after_.chezmoitmp_home_Alexx2000.asc.sh
@@ -4,3 +4,9 @@ SRC="$HOME/.chezmoitmp/home_Alexx2000.asc"
 DST="/etc/apt/trusted.gpg.d/home_Alexx2000.gpg"
 sudo gpg -v --dearmor --output "$DST" "$SRC"
 
+echo "Adding custom repository /etc/apt/sources.list.d/..."
+sudo tee -a /etc/apt/sources.list.d/home\:Alexx2000.list > /dev/null <<EOL
+# Backport repository
+deb http://download.opensuse.org/repositories/home:/Alexx2000/Debian_12/ /
+fi
+EOL
--- home/.chezmoiscripts/run_once_after_.chezmoitmp_suldr-keyring.asc.sh
+++ home/.chezmoiscripts/run_once_after_.chezmoitmp_suldr-keyring.asc.sh
@@ -3,3 +3,12 @@
 SRC="$HOME/.chezmoitmp/suldr-keyring.asc"
 DST="/etc/apt/trusted.gpg.d/suldr-keyring.gpg"
 sudo gpg -v --dearmor --output "$DST" "$SRC"
+
+echo "Adding custom repository /etc/apt/sources.list.d/..."
+sudo tee -a /etc/apt/sources.list.d/home\:Alexx2000.list > /dev/null <<EOL
+# Backport repository
+deb https://www.bchemnet.com/suldr/ debian extra
+fi
+EOL
+
+
