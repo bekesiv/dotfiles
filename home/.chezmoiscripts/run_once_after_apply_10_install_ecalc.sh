11d89fc9b3814e92edcef4049018447adb2d0924
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-16 22:42:21 +0200

    Fix directory check for eCalc installation script

--- home/.chezmoiscripts/run_once_after_apply_10_install_ecalc.sh
+++ home/.chezmoiscripts/run_once_after_apply_10_install_ecalc.sh
@@ -3,7 +3,7 @@ set -e
 
 # eCalc
 github_dir="$HOME/work/github"
-if [ ! -d "$github_dir" ]; then
+if [ ! -d "$github_dir/ecalc" ]; then
     mkdir -p "$github_dir"
     cd "$github_dir"
     git clone git@github.com:bekesiv/ecalc.git
