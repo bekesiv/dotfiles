63089206078bd082a3fe5c55c60f9dacb36c7fcc
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-16 23:05:17 +0200

    Refactor .chezmoiexternal.toml to remove duplicate OpenZone_White entry and add brave_browser_install.sh script; update run_once_before_apply_30_install_flatpaks.sh to remove unused flatpak uninstall command

---
+++ home/.chezmoiscripts/run_once_after_.chezmoiinstall_brave_browser_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+cd $HOME/.chezmoiinstall
+sh brave_browser_install.sh
--- home/.chezmoiscripts/run_once_before_apply_30_install_flatpaks.sh
+++ home/.chezmoiscripts/run_once_before_apply_30_install_flatpaks.sh
@@ -5,7 +5,6 @@ files_dir="$CHEZMOI_SOURCE_DIR/files"
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y $(tr '\n' ' ' < "$files_dir/flatpak.list")
-flatpak uninstall --unused
 
 # Flatpak does not respect gtk cursors
 flatpak --user override --filesystem=$HOME/.icons/:ro
