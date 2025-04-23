c3550ba151b1e44d4c2f5524d99541e94b3317ec
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-24 00:21:08 +0200

    Add shebang and set -e to scripts for TCP buffer tuning and time synchronization

--- home/.chezmoiscripts/run_once_after_apply_97_tune_tcp_receive_buffer_settings.sh
+++ home/.chezmoiscripts/run_once_after_apply_97_tune_tcp_receive_buffer_settings.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+set -e
+
 echo -e "net.ipv4.tcp_rmem=40960 873800 62914560\nnet.core.rmem_max=25000000" | sudo tee -a /etc/sysctl.conf > /dev/null
--- home/.chezmoiscripts/run_once_after_apply_98_install_timesyncd.sh
+++ home/.chezmoiscripts/run_once_after_apply_98_install_timesyncd.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+set -e
+
 sudo apt install systemd-timesyncd
 sudo systemctl enable --now systemd-timesyncd
