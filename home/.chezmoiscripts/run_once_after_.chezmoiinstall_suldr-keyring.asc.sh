8a17d66c26a692bb1377b784e019da875e70275c
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-08-18 01:25:30 +0200

    Add TeamViewer repo file list to TODO; update suldr-keyring handling and add encrypted file

--- home/.chezmoiscripts/run_once_after_.chezmoiinstall_suldr-keyring.asc.sh
+++ home/.chezmoiscripts/run_once_after_.chezmoiinstall_suldr-keyring.asc.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-SRC="$HOME/.chezmoiinstall/suldr-keyring.asc"
-DST="/etc/apt/trusted.gpg.d/suldr-keyring.gpg"
-sudo gpg -v --dearmor --output "$DST" "$SRC"
+SRC="$HOME/.chezmoiinstall/suldr-keyring.gpg"
+DST="/etc/apt/trusted.gpg.d"
+DSFTILE="$DST/suldr-keyring.gpg"
+# sudo gpg -v --dearmor --output "$DST" "$SRC"
+sudo mkdir -p "$DST"
+sudo install -v -o root -g root -m 644 "$SRC" "$DST/"
 
-echo "Adding custom repository $DST..."
+echo "Adding custom repository $DSFTILE..."
 sudo mkdir -p /etc/apt/sources.list.d
 sudo tee -a /etc/apt/sources.list.d/suldr.sources > /dev/null <<EOL
 Types: deb
 URIs: https://www.bchemnet.com/suldr/
 Suites: debian
 Components: extra
-Signed-By: $DST
+Signed-By: $DSTFILE
 EOL
 
 sudo apt update
