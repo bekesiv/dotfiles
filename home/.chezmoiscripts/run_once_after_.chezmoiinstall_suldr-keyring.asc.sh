baafb23ea620c5ad182a6a2934bb35d68a326593
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-08-18 08:02:32 +0200

    Update suldr-keyring handling: replace GPG with DEB installation and remove encrypted files

--- home/.chezmoiscripts/run_once_after_.chezmoiinstall_suldr-keyring.asc.sh
+++ home/.chezmoiscripts/run_once_after_.chezmoiinstall_suldr-keyring.asc.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-SRC="$HOME/.chezmoiinstall/suldr-keyring.gpg"
-DST="/etc/apt/trusted.gpg.d"
-DSFTILE="$DST/suldr-keyring.gpg"
-# sudo gpg -v --dearmor --output "$DST" "$SRC"
-sudo mkdir -p "$DST"
-sudo install -v -o root -g root -m 644 "$SRC" "$DST/"
+SRC="$HOME/.chezmoiinstall/suldr-keyring.deb"
+DST="/etc/apt/trusted.gpg.d/suldr-keyring.gpg"
 
-echo "Adding custom repository $DSFTILE..."
+echo "Adding custom repository $DST..."
+sudo dpkg -i "$SRC"
 sudo mkdir -p /etc/apt/sources.list.d
 sudo tee -a /etc/apt/sources.list.d/suldr.sources > /dev/null <<EOL
 Types: deb
 URIs: https://www.bchemnet.com/suldr/
 Suites: debian
 Components: extra
-Signed-By: $DSTFILE
+Signed-By: $DST
 EOL
 
 sudo apt update
-sudo apt install -y suld-driver2-1.00.39
+sudo apt install -y suld-driver2-1.00.36
